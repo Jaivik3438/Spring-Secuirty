23c8367728913da01ca650c0c46b959bcfd74527
Jaivik Tailor <jaiviktailor@Jaiviks-MacBook-Pro.local>
2025-03-12 18:02:49 -0400

    Added CSRF Tocken and use that for POST and GET

--- src/main/java/com/jaivik/SpringSecurityExample/controllers/HelloController.java
+++ src/main/java/com/jaivik/SpringSecurityExample/controllers/HelloController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HelloController {
     
     @GetMapping("/")
-    public String greet(){
+    public String greet(HttpServletRequest httpServletRequest){
 
-        return "Hello";
+        return "Hello Guys" + httpServletRequest.getSession().getId();
     } 
 }
